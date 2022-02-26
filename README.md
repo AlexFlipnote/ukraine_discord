1920ca781d3917af61aa5df474868a42ea743436
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-02-26 21:23:55 +0000

    First release!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ukraine_discord
 A script that gets the news from liveuamap.com to post is on Discord as a webhook
+
+# Live view
+The bot is currently running and posting news right now as an example inside [my personal Discord server](https://discord.gg/AlexFlipnote), inside the channel `#general_whatever` within the thread called `Ukraine Vs Russia - Discussion and News`.
+
+# Requirements
+- Python 3.6 or higher
+- Discord webhook URL
+
+## Example outputs
+![](https://i.alexflipnote.dev/A2eAB5G.png)
+
+![](https://i.alexflipnote.dev/7CvYAAB.png)
---
+++ index.py
@@ -0,0 +1,183 @@
+import requests
+import json
+import time
+import traceback
+import random
+
+from datetime import datetime
+from colorama import Fore, Style
+from bs4 import BeautifulSoup
+
+
+def traceback_maker(err):
+    """ Make a traceback from the error """
+    _traceback = ''.join(traceback.format_tb(err.__traceback__))
+    error = ('{1}{0}: {2}').format(type(err).__name__, _traceback, err)
+    return error
+
+
+class Feed:
+    def __init__(self, html_data):
+        self.html = html_data
+        self.info = html_data.find("div", {"class": "title"}).text
+        self.id = html_data.attrs.get("data-id", None)
+        self.extra = html_data.find("a", {"data-id": self.id, "class": "comment-link"}).attrs.get("href", None)
+
+    @property
+    def video(self):
+        """ Returns the video url if available """
+        find_video = self.html.find("blockquote", {"class": "twitter-video"})
+        if not find_video:
+            return None
+        return find_video.find("a").attrs.get("href", None)
+
+    @property
+    def feed_img(self):
+        """ Get the image of the feed """
+        find_img = self.html.find("div", {"class": "img"})
+        if not find_img:
+            return None
+        get_img = find_img.find("img")
+        if not get_img:
+            return None
+        return get_img.attrs.get("src", None)
+
+
+class Article:
+    def __init__(self, feed: Feed, html_data):
+        self.html = html_data
+        self.feed = feed
+
+        self.info = feed.info
+        self.id = feed.id
+        self.extra = feed.extra
+        self.video = feed.video
+        self.feed_img = feed.feed_img
+
+    @property
+    def source(self):
+        """ Get the source of the article """
+        html = self.html.find("a", {"class": "source-link"})
+        if not html:
+            return None
+        return html.attrs.get("href", None)
+
+    @property
+    def image(self):
+        """ Get the image from the article """
+        has_img = self.html.find("div", {"class": "popup_imgi"})
+        if not has_img:
+            return None
+        get_img = has_img.find("img")
+        if not get_img:
+            return None
+        return get_img.attrs.get("src", None)
+
+
+def read_json():
+    """ Read the config.json file """
+    with open("./config.json", "r") as f:
+        return json.load(f)
+
+
+def write_json(**kwargs):
+    """ Use the config.json to write to the file """
+    data = read_json()
+    for key, value in kwargs.items():
+        data[key] = value
+    with open("./config.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def webhook(html_content: Article):
+    """ Send webhook to Discord """
+    embed = {
+        "title": "New update",
+        "color": 0xf1c40f,
+        "thumbnail": {"url": "https://cdn.discordapp.com/emojis/691373958087442486.png"}
+    }
+
+    if html_content.source:
+        embed["description"] = f"[ℹ️ Source of the news]({html_content.source})\n{html_content.info}"
+    else:
+        embed["description"] = f"ℹ️ Unable to find source...\n{html_content.info}"
+
+    image = html_content.image or html_content.feed_img or None
+    if image:
+        embed["image"] = {"url": image}
+    if html_content.video:
+        embed["description"] += f"\n\n> Warning: Can be graphical, view at own risk\n[Twitter video]({html_content.video})"
+
+    return requests.post(
+        read_json()["webhook_url"],
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({"content": None, "embeds": [embed]}),
+    )
+
+
+def pretty_print(symbol: str, text: str):
+    """ Use colorama to print text in pretty colours """
+    data = {
+        "+": Fore.GREEN, "-": Fore.RED,
+        "!": Fore.YELLOW, "?": Fore.CYAN,
+    }
+
+    colour = data.get(symbol, Fore.WHITE)
+    print(f"{colour}[{symbol}]{Style.RESET_ALL} {text}")
+
+
+def fetch(url: str):
+    """ Simply fetch any URL given, and convert from bytes to string """
+    r = requests.get(
+        url, headers={
+            "User-Agent": read_json()["user_agent"],
+            "Content-Type": "text/html; charset=UTF-8",
+        }
+    )
+    text = r.content.decode("utf-8")
+    return text
+
+
+def main():
+    while True:
+        try:
+            # Random wait time to not be too obvious
+            # Since we are scraping the website, lol
+            check_in_rand = random.randint(45, 75)
+
+            pretty_print("?", f"{datetime.now()} - Checking for new articles")
+            pretty_print("+", "Fetching all articles and parsing HTML...")
+
+            r = fetch("https://liveuamap.com/")
+            html = BeautifulSoup(r, "html.parser")
+
+            try:
+                feeder = html.find("div", {"id": "feedler"})
+                latest_news = next((g for g in feeder), None)
+            except TypeError:
+                # For some weird reason, this website loves to crash with HTTP 5XX
+                # So we just try again because the website encourages us to, really.
+                pretty_print("!", "Failed to get feeder, probably 500 error, trying again...")
+                time.sleep(5)
+                continue
+
+            news = Feed(latest_news)
+            if news.id != read_json()["last_id"]:
+                pretty_print("+", "New article found, checking article...")
+                r_extra = fetch(news.extra)
+                extra_html = BeautifulSoup(r_extra, "html.parser")
+                webhook(Article(news, extra_html))
+                write_json(last_id=news.id)
+                pretty_print("!", news.info)
+            else:
+                pretty_print("-", f"Found no news... waiting {check_in_rand} seconds")
+        except Exception as e:
+            pretty_print("!", traceback_maker(e))
+
+        time.sleep(check_in_rand)
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    pretty_print("!", "Exiting...")
